cfd15acf1252865d17f7e573fd2690dc61fe2c64
OxyEho <valera808@inbox.ru>
2022-01-12 20:46:19 +0500

    смне конфига

--- pub_proj/settings.py
+++ pub_proj/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
